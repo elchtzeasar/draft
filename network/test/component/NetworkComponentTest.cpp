73d89b927fb2d55e53e66da35d4cc9d7b5762f16
peter <peter@peter-virtual.(none)>
2011-04-28 15:35:30 +0200

    Refactored component tests for network component.

--- network/test/component/NetworkComponentTest.cpp
+++ network/test/component/NetworkComponentTest.cpp
@@ -13,117 +13,165 @@
 
 #include <gtest/gtest.h>
 
+class ComponentWrapper {
+public:
+    ComponentWrapper(const PlayerId playerId);
+    ~ComponentWrapper();
+
+    void host();
+
+    void connectToServerAndWait();
+
+    const PlayerId& getPlayerId() const;
+    NetworkComponent& getNetworkComponent();
+    QSignalSpy& getClientConnectedSpy();
+    const SignalWaiter& getClientConnectedWaiter() const;
+    QSignalSpy& getConnectedToServerSpy();
+    const SignalWaiter& getConnectedToServerWaiter() const;
+    QSignalSpy& getDataReceivedSpy();
+    const SignalWaiter& getDataReceivedWaiter() const;
+private:
+    static const unsigned int PORT;
+    static const QString LOCALHOST;
+
+    const PlayerId playerId;
+    NetworkComponentFactoryImpl networkComponentFactory;
+    NetworkComponent* networkComponent;
+
+    QSignalSpy clientConnectedSpy;
+    SignalWaiter clientConnectedWaiter;
+    QSignalSpy connectedToServerSpy;
+    SignalWaiter connectedToServerWaiter;
+    QSignalSpy dataReceivedSpy;
+    SignalWaiter dataReceivedWaiter;
+};
+
+const unsigned int ComponentWrapper::PORT(12345);
+const QString ComponentWrapper::LOCALHOST("localhost");
+
+ComponentWrapper::ComponentWrapper(const PlayerId playerId) :
+	        playerId(playerId),
+	        networkComponentFactory(),
+	        networkComponent(networkComponentFactory.createComponent()),
+
+	        clientConnectedSpy(networkComponent, SIGNAL(clientConnected(const PlayerId&))),
+	        clientConnectedWaiter(clientConnectedSpy),
+	        connectedToServerSpy(networkComponent, SIGNAL(connectedToDraft())),
+	        connectedToServerWaiter(connectedToServerSpy),
+	        dataReceivedSpy(networkComponent, SIGNAL(dataReceived(const AddressedMessage&))),
+	        dataReceivedWaiter(dataReceivedSpy) {
+}
+
+ComponentWrapper::~ComponentWrapper() {
+    delete networkComponent;
+}
+
+const PlayerId& ComponentWrapper::getPlayerId() const {
+    return playerId;
+}
+NetworkComponent& ComponentWrapper::getNetworkComponent() {
+    return *networkComponent;
+}
+QSignalSpy& ComponentWrapper::getClientConnectedSpy() {
+    return clientConnectedSpy;
+}
+const SignalWaiter& ComponentWrapper::getClientConnectedWaiter() const {
+    return clientConnectedWaiter;
+}
+QSignalSpy& ComponentWrapper::getConnectedToServerSpy() {
+    return connectedToServerSpy;
+}
+const SignalWaiter& ComponentWrapper::getConnectedToServerWaiter() const {
+    return connectedToServerWaiter;
+}
+QSignalSpy& ComponentWrapper::getDataReceivedSpy() {
+    return dataReceivedSpy;
+}
+const SignalWaiter& ComponentWrapper::getDataReceivedWaiter() const {
+    return dataReceivedWaiter;
+}
+
+void ComponentWrapper::host() {
+    networkComponent->handleHostDraft(PORT);
+}
+
+void ComponentWrapper::connectToServerAndWait() {
+    networkComponent->handleConnectToDraft(LOCALHOST, PORT);
+
+    clientConnectedWaiter.wait();
+}
+
+
 class NetworkComponentTest : public testing::Test {
 public:
-  NetworkComponentTest() : 
-    serverPlayerId(PlayerId::SERVER),
-    serverNetworkComponent(serverNetworkComponentFactory.createComponent()),
-    clientConnectedSpy(serverNetworkComponent, SIGNAL(clientConnected(const PlayerId&))),
-    clientConnectedWaiter(clientConnectedSpy),
-
-    clientPlayerId(0),
-    clientNetworkComponent(clientNetworkComponentFactory.createComponent()),
-    connectedToServerSpy(clientNetworkComponent, SIGNAL(connectedToDraft())),
-    connectedToServerWaiter(connectedToServerSpy) {}
-
-  ~NetworkComponentTest() {
-    delete serverNetworkComponent;
-    delete clientNetworkComponent;
-  }
+    NetworkComponentTest() :
+        server(PlayerId::SERVER),
+        client(0) {}
 
 protected:
 
-  void hostServer() {
-    serverNetworkComponent->handleHostDraft(PORT);
-  }
+    AddressedMessage createAddressedMessage(const PlayerId& senderPlayerId,
+                                            const PlayerId& receiverPlayerId) {
+        AddressHeader* header(new AddressHeader(senderPlayerId, receiverPlayerId));
+        NullMessage* nullMessage(new NullMessage(MessageNumber::NULL_MESSAGE));
+        return AddressedMessage(header, nullMessage);
+    }
 
-  void connectClientToServerAndWait(NetworkComponent& client) {
-    client.handleConnectToDraft(LOCALHOST, PORT);
-
-    clientConnectedWaiter.wait();
-  }
-
-  AddressedMessage createAddressedMessage(const PlayerId& senderPlayerId,
-				    const PlayerId& receiverPlayerId) {
-    AddressHeader* header(new AddressHeader(senderPlayerId, receiverPlayerId));
-    NullMessage* nullMessage(new NullMessage(MessageNumber::NULL_MESSAGE));
-    return AddressedMessage(header, nullMessage);
-  }
-
-  static const unsigned int PORT;
-  static const QString LOCALHOST;
-
-  const PlayerId serverPlayerId;
-  NetworkComponentFactoryImpl serverNetworkComponentFactory;
-  NetworkComponent* serverNetworkComponent;
-  QSignalSpy clientConnectedSpy;
-  SignalWaiter clientConnectedWaiter;
-
-  const PlayerId clientPlayerId;
-  NetworkComponentFactoryImpl clientNetworkComponentFactory;
-  NetworkComponent* clientNetworkComponent;
-  QSignalSpy connectedToServerSpy;
-  SignalWaiter connectedToServerWaiter;
+    ComponentWrapper server;
+    ComponentWrapper client;
 };
 
-const unsigned int NetworkComponentTest::PORT(12345);
-const QString NetworkComponentTest::LOCALHOST("localhost");
-
 TEST_F(NetworkComponentTest, shouldEmitClientConnectedInServerWhenClientConnects) {
-  hostServer();
-  connectClientToServerAndWait(*clientNetworkComponent);
+    server.host();
+    client.connectToServerAndWait();
 
-  clientConnectedWaiter.wait();
-  ASSERT_EQ(1, clientConnectedSpy.count());
+    server.getClientConnectedWaiter().wait();
+    ASSERT_EQ(1, server.getClientConnectedSpy().count());
 
-  QList<QVariant> arguments = clientConnectedSpy.takeFirst();
-  const PlayerId& clientAssignedPlayerId(arguments.at(0).value<PlayerId>());
-  ASSERT_EQ(clientPlayerId, clientAssignedPlayerId);
+    QList<QVariant> arguments = server.getClientConnectedSpy().takeFirst();
+    const PlayerId& clientAssignedPlayerId(arguments.at(0).value<PlayerId>());
+    ASSERT_EQ(client.getPlayerId(), clientAssignedPlayerId);
 }
 
 TEST_F(NetworkComponentTest, shouldEmitConnectedToDraftInClientWhenConnectedToServer) {
-  hostServer();
-  connectClientToServerAndWait(*clientNetworkComponent);
+    server.host();
+    client.connectToServerAndWait();
 
-  ASSERT_EQ(1, connectedToServerSpy.count());
+    ASSERT_EQ(1, client.getConnectedToServerSpy().count());
 }
 
 TEST_F(NetworkComponentTest, shouldSendDataFromServerToClientCorrectly) {
-  QSignalSpy dataReceivedInClientSpy(
-    clientNetworkComponent, SIGNAL(dataReceived(const AddressedMessage&)));
-  SignalWaiter dataReceivedInClientWaiter(dataReceivedInClientSpy);
-  const AddressedMessage addressedMessageToSend(
-    createAddressedMessage(serverPlayerId, clientPlayerId));
+    const AddressedMessage addressedMessageToSend(
+            createAddressedMessage(server.getPlayerId(),
+                    client.getPlayerId()));
 
-  hostServer();
-  connectClientToServerAndWait(*clientNetworkComponent);
+    server.host();
+    client.connectToServerAndWait();
 
-  serverNetworkComponent->handleSendData(addressedMessageToSend);
+    server.getNetworkComponent().handleSendData(addressedMessageToSend);
 
-  dataReceivedInClientWaiter.wait();
-  ASSERT_EQ(1, dataReceivedInClientSpy.count());
+    client.getDataReceivedWaiter().wait();
+    ASSERT_EQ(1, client.getDataReceivedSpy().count());
 
-  QList<QVariant> arguments = dataReceivedInClientSpy.takeFirst();
-  const AddressedMessage& receivedAddressedMessage(arguments.at(0).value<AddressedMessage>());
-  ASSERT_EQ(addressedMessageToSend, receivedAddressedMessage);
+    QList<QVariant> arguments = client.getDataReceivedSpy().takeFirst();
+    const AddressedMessage& receivedAddressedMessage(arguments.at(0).value<AddressedMessage>());
+    ASSERT_EQ(addressedMessageToSend, receivedAddressedMessage);
 }
 
 TEST_F(NetworkComponentTest, shouldSendDataFromClientToServerCorrectly) {
-  QSignalSpy dataReceivedInServerSpy(
-    serverNetworkComponent, SIGNAL(dataReceived(const AddressedMessage&)));
-  SignalWaiter dataReceivedInServerWaiter(dataReceivedInServerSpy);
-  const AddressedMessage addressedMessageToSend(
-    createAddressedMessage(clientPlayerId, serverPlayerId));
+    const AddressedMessage addressedMessageToSend(
+            createAddressedMessage(client.getPlayerId(),
+                    server.getPlayerId()));
 
-  hostServer();
-  connectClientToServerAndWait(*clientNetworkComponent);
+    server.host();
+    client.connectToServerAndWait();
 
-  clientNetworkComponent->handleSendData(addressedMessageToSend);
+    client.getNetworkComponent().handleSendData(addressedMessageToSend);
 
-  dataReceivedInServerWaiter.wait();
-  ASSERT_EQ(1, dataReceivedInServerSpy.count());
+    server.getDataReceivedWaiter().wait();
+    ASSERT_EQ(1, server.getDataReceivedSpy().count());
 
-  QList<QVariant> arguments = dataReceivedInServerSpy.takeFirst();
-  const AddressedMessage& receivedAddressedMessage(arguments.at(0).value<AddressedMessage>());
-  ASSERT_EQ(addressedMessageToSend, receivedAddressedMessage);
+    QList<QVariant> arguments = server.getDataReceivedSpy().takeFirst();
+    const AddressedMessage& receivedAddressedMessage(arguments.at(0).value<AddressedMessage>());
+    ASSERT_EQ(addressedMessageToSend, receivedAddressedMessage);
 }
